a0a19edd19525645bb42a6484a0c400243956a44
20odeebh <31841180+20odeebh@users.noreply.github.com>
2020-08-20 19:26:59 +0900

    Create 0820 사탕가게.cpp
    백준 사탕가게

---
+++ 0820 사탕가게.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, c,dp[10001];
+double m, p;
+
+int main(){
+	while (true) {
+		memset(dp, 0, sizeof(dp));
+		cin >> n >> m;
+		if (n == 0 && m == 0)break;
+		int money = (int)(m * 100 + 0.5);
+		for (int i = 0; i < n; i++) {
+			cin >> c >> p;
+			int price=(int)(p * 100 + 0.5);
+			for (int j = price; j <= money; j++) {
+				dp[j] = max(dp[j], dp[j - price] + c);
+			}
+		}
+		cout << dp[money]<<"\n";
+	}
+}
