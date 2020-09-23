b72c3eebd5127096c08f73614402328675c084e5
20odeebh <31841180+20odeebh@users.noreply.github.com>
2020-09-23 22:46:47 +0900

    BOJ_ROT 23

---
+++ 0923 ROT23.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main()
+{
+    getline(cin, s);
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 'a' && s[i] <= 'z' ) 
+        {
+            if(s[i]+13>'z') s[i] = s[i] +13-26;
+            else s[i] = s[i] + 13;
+        }
+        if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            if (s[i] + 13 > 'Z') s[i] = s[i] + 13 - 26;
+            else s[i] = s[i] + 13;
+        }
+    }
+    cout << s;
+}
